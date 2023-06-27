8fd8adf06b41f3365482794dce608f7d53dd4a0c
Akash Chondra Roy <97385572+Akash0467@users.noreply.github.com>
2023-06-28 00:42:23 +0600

    change
    change

--- assets/index.js
+++ assets/index.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
     $('.click-down').click(function(){
         $('.menu').toggleClass("other");
     });
+
+    // Click Event
+    const toggleBtn = document.querySelector(".show");
+    const toggleBtnIcon = document.querySelector(".show i");
+    const dropDownMenu = document.querySelector(".menu");
+    toggleBtn.onclick = function(){
+        dropDownMenu.classList.toggle('open')
+        const isOpen = dropDownMenu.classList.contains('open')
+
+        toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+    }
+
 });
